dcebe229112b28324fc3fbd58934b9fa9aad978c
Orochii481 <83424384+Orochii481@users.noreply.github.com>
2021-04-29 17:15:20 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,190 @@
+const help = (prefix) => {
+	return `
+<═══════════════════>
+      *§OROCHII§*
+ <══════════════════>
+
+__█████████  ● ᏴϴͲ●ᎷᎬΝႮ●         
+__█▄█████▄█   *DONO* OROCHII
+__█▼▼▼▼▼█ 
+_██ᏴϴͲ Ꮩ3.8██▌ 
+__█▲▲▲▲▲█ 
+__█████████ 
+____██_____██
+
+┌─────────❶.❷────────
+║〘 INFORMAÇÕES 〙
+║
+╠🐉 *§OROCHII§* 
+╠🐉 𝐃𝐎𝐍𝐎:  ⃬⃗OROCHII(base dark) 
+╠🐉 *wa.me/+5547992091566*
+╠🐉 𝐒𝐓𝐀𝐓𝐔𝐒: ON
+║
+║
+╠══🐰〘 MENU 〙🐰══
+║
+║🐍 *${prefix}figu*
+║🐍 *${prefix}toimg*
+║🐍 *${prefix}tts*
+║🐍 *${prefix}lolih [on]*
+║🐍 *${prefix}nsfwloli [off]*
+║🐍 *${prefix}url2img*
+║🐍 *${prefix}leens [na legenda]*
+║🐍 *${prefix}wait [na legenda]*
+║🐍 *${prefix}setprefix*
+║
+╠══🐰〘 IMAGENS 〙🐰══
+
+║🐍 *${prefix}boanoite*
+║🐍 *${prefix}bomdia*
+║🐍 *${prefix}boatarde
+║🐍 *${prefix}rize [aleatórias]*
+║🐍 *${prefix}minato [aleatórias]*
+║🐍 *${prefix}boruto [aleatórias]*
+║🐍 *${prefix}sasuke [aleatórias]*
+║🐍 *${prefix}sakura [aleatórias]*
+║🐍 *${prefix}naruto [aleatórias]*
+║🐍 *${prefix}meme*  
+║
+╠══🐰〘 INTELIGÊNCIA 〙🐰══
+║
+║🐍 *${prefix}simih 1 (para ativar)*
+║🐍 *${prefix}simih 0 (para desativar)*
+║ *${prefix}simi (sua mensagem)*
+║
+╠══🐰〘 EM PRODUÇÃO 〙🐰══
+║
+║🐍 *${prefix}gado*
+║🐍 *${prefix}dbz*
+║🐍 *${prefix}gtts loli*
+║🐍 *${prefix}hidegp*
+║🐍 *${prefix}grupos*
+║🐍 *${prefix}antilink*
+║🐍 *${prefix}it 1/0
+║
+╠══🐰〘 SÓ PREMIUM 〙🐰══
+║
+║🐍 *${prefix}dado*
+║🐍 *${prefix}cekvip*
+║🐍 *${prefix}premiumlist*
+║🐍 *${prefix}delete*
+║🐍 *${prefix}modapk*
+║🐍 *${prefix}indo10*
+║🐍 *${prefix}qrcode*
+║🐍 *${prefix}chentai*
+║🐍 *${prefix}gcpf*
+║🐍 *${prefix}gbin*
+║🐍 *${prefix}pack*
+║🐍 *${prefix}destrava*
+║🐍 *${prefix}gpessoa*
+║
+╠══🐰〘 GRUPO 〙🐰══
+║
+║🐍 *${prefix}banir*
+║🐍 *${prefix}leveling [on/off]*
+║🐍 *${prefix}level*
+║🐍 *${prefix}add*
+║🐍 *${prefix}promover*
+║🐍 *${prefix}setfoto [na legenda]*
+║🐍 *${prefix}setname [texto]*
+║🐍 *${prefix}rebaixar*
+║🐍 *${prefix}admins*
+║🐍 *${prefix}marcar*
+║🐍 *${prefix}marcar2*
+║🐍 *${prefix}marcar3*
+║🐍 *${prefix}bemvindo [1/0]*
+║🐍 *${prefix}grupoinfo*
+║🐍 *${prefix}bomdia*
+║🐍 *${prefix}boatarde*
+║🐍 *${prefix}closegc*
+║🐍 *${prefix}setdesc*
+║🐍 *${prefix}opengc*
+║🐍 *${prefix}linkgp*
+║🐍 *${prefix}nomeg(gc)*
+║
+╠══🐰〘 ESPECIFICO DO BOT 〙🐰══
+║
+║🐍 *${prefix}bug [sua mensagem]*
+║🐍 *${prefix}clonar [@]*
+║🐍 *${prefix}ping [ver velocidade do bot]*
+║🐍 *${prefix}termux*
+║🐍 *${prefix}gay [@]*
+║🐍 *${prefix}wame*
+║🐍 *${prefix}map (nome)*
+║🐍 *${prefix}setppbot (marque uma img)*
+║🐍 *${prefix}pinterest (nome)*
+║🐍 *${prefix}desligar (so para o dono)*
+║🐍 *${prefix}timer*
+║🐍 *${prefix}punheteiros*
+║🐍 *${prefix}gados*
+║
+╠══🐰〘 MAIS ALGUNS 〙🐰══
+║
+║🐍 *${prefix}neko*
+║🐍 *${prefix}ttp [texto]*
+║🐍 *${prefix}testime*
+║🐍 *${prefix}tomp3*
+║🐍 *${prefix}modoanime [on/off]*
+║🐍 *${prefix}modonsfw [on/off]*
+║🐍 *${prefix}happymod [jogo/app]*
+║🐍 *${prefix}rize*
+║🐍 *${prefix}ytsearch*
+║🐍 *${prefix}moddroid [jogo/app]*
+║🐍 *${prefix}gay1*
+║🐍 *${prefix}next*
+║🐍 *${prefix}alerta*
+║🐍 *${prefix}pronomeneu [texto]*
+║🐍 *${prefix}hobby*
+║🐍 *${prefix}kiss
+║🐍 *${prefix}analise
+║
+╠══🐰〘 COMANDOS DE VOZ 〙🐰══
+║
+║🐍 *${prefix}ola*
+║🐍 *${prefix}bv*
+║🐍 *${prefix}tchau*
+║🐍 *${prefix}bem*
+║🐍 *${prefix}a*
+║🐍 *${prefix}fdp*
+║🐍 *${prefix}onich*
+║🐍 *${prefix}beat1*
+║🐍 *${prefix}glub*
+║🐍 *${prefix}causs*
+║
+╠══🐰〘 OUTROS /2 〙🐰══
+║
+║🐍 *${prefix}antilink [1/0]*
+║🐍 *${prefix}brainly [pergunta]*
+║🐍 *${prefix}antiracismo [on/off]*
+║🐍 *${prefix}setnomebot*
+║🐍 *${prefix}meme*
+║
+╠══🐰〘 INTERATIVOS 〙🐰══
+║
+╠══NOTA »
+║Mandar a msg sem o prefixo
+╠════════════════════
+║
+║🐍 *bah*
+║🐍 *oii*
+║🐍 *bv*
+║🐍 *canta ai bot*
+║🐍 *grita*
+║🐍 *causs*
+║🐍 *gemidao*
+║🐍 *musica*
+║
+╠══🐰〘 NO CONTROLE 〙🐰══
+║
+║ *NOME: OROCHII*
+║ *WPP: wa.me/+55
+║
+║  *"base do lindo (DARK BOT 4.0)🐍🚩*
+║  *"kiba não xereka"*
+║  *Número do dark: wa.me/+5522996215481
+║
+╚═〘 §OROCHII§〙`
+}
+
+exports.help = help
+
