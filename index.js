c6add3fd646d70e647c032264c7bf87cb0e8f4c4
Orochii481 <83424384+Orochii481@users.noreply.github.com>
2021-05-12 12:24:45 -0300

    Update index.js

--- index.js
+++ index.js
@@ -751,7 +751,7 @@ if (text.includes("placa"))
 				case 'menu':
 			me=client.user
 			uptime=process.uptime		
-			client.sendMessage(from, help(pushname,prefix), text, {quoted: mek, quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(from ? { remoteJid: "status@broadcast" } : {}) }, message: { "imageMessage": { "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc", "mimetype": "image/jpeg","caption": "*CAUSS DOMINA VADIA*", 'jpegThumbnail': fs.readFileSync('./assets/botlogo.webp')}}}})
+			client.sendMessage(from, help(pushname,prefix), text, {quoted: mek, quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(from ? { remoteJid: "status@broadcast" } : {}) }, message: { "imageMessage": { "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc", "mimetype": "image/jpeg","caption": "*OROCHII*", 'jpegThumbnail': fs.readFileSync('./assets/botlogo.webp')}}}})
                   
 		break	
 			case 'help1':
