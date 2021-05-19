82a8c66e04f8587fdd9ce20d4bf6a644e7194b83
Orochii481 <83424384+Orochii481@users.noreply.github.com>
2021-05-19 10:16:59 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,308 @@
+const help = (pushname, prefix) => { 
+
+	return `
+<══════════════════════>
+      *CAUSS BOT*
+ <══════════════════════>
+
+__█████████  ● ᏴϴͲ●ᎷᎬΝႮ●         
+__█▄█████▄█   
+__█▼▼▼▼▼█ 
+_██ᏴϴͲ Ꮩ4.0██▌  
+__█▲▲▲▲▲█ 
+__█████████ 
+____██_____██
+
+┌─────────❶.❷────────
+┏━━━━━━━━━━━━
+┣ ⚜️ | Prefix: 「 ${prefix} 」
+┣ ⚜️ | Status: Online
+┣ ⚜️ | By: Causs
+┗━━━━━━━━━━━━
+
+┏━━「 User」 ━━━
+┣ ⚜️ | nome: ${pushname}
+┣ ⚜️ | Limite: 9999999
+┣ ⚜️ | total: (não definido) 
+┣ ⚜️ | Registro: nop
+┗━━━━━━━━━━━━
+
+┌─────────❶.❷────────
+║
+╠══〘 MENU 〙══
+║
+║🌕 */figu*
+║🌕 */toimg*
+║🌕 */caussmeme*
+║🌕 */memeindo*
+║🌕 */tts*
+║🌕 */lolih [on]*
+║🌕 */nsfwloli [off]*
+║🌕 */url2img*
+║🌕 */leens [na legenda]*
+║🌕 */wait [na legenda]*
+║🌕 */setprefix*
+║🌕 */linkgp*
+║🌕 */simih [1/0]*
+║🌕 */marcar*
+║🌕 */add [@]*
+║🌕 */banir [@]*
+║🌕 */promover [@]*
+║🌕 */rebaixar*
+║🌕 */admins*
+║🌕 */marcar2*
+║🌕 */bc [texto]* (ele faz uma ™)
+║🌕 */marcar3*
+║🌕 */bloqueados*
+║🌕 */bloquear [@]*
+║🌕 */desbloquear [@]*
+║🌕 */limpar*
+║🌕 */bc [ *texto* ]*
+║🌕 */bemvindo [1/0]*
+║🌕 */clonar [@]*
+║🌕 */help1*
+║🌕 */dono*
+║🌕 */owner*
+║🌕 */tts [texto]*
+║🌕 */setnome*
+║🌕 */termux*
+║🌕 */setfoto*
+║🌕 */grupoinfo*
+║🌕 */ytmp4*
+║🌕 */bomdia*
+║🌕 */boanoite*
+║🌕 */marcar*
+║🌕 */marcar2*
+║🌕 */marcar3*
+║🌕 */hidetag
+║🌕 */hidetag2
+║🌕 */gostosa 
+║🌕 */top5viado
+║
+╠══〘 GRUPO 〙══
+║
+║🌕 */banir*
+║🌕 */leveling [on/off]*
+║🌕 */level*
+║🌕 */add*
+║🌕 */promover*
+║🌕 */setfoto [na legenda]*
+║🌕 */setname [texto]*
+║🌕 */rebaixar*
+║🌕 */admins*
+║🌕 */marcar*
+║🌕 */marcar2*
+║🌕 */marcar3*
+║🌕 */bemvindo [1/0]*
+║🌕 */grupoinfo*
+║🌕 */bomdia*
+║🌕 */boatarde*
+║🌕 */boanoite*
+║🌕 */setdesc*
+║🌕 */bug [sua mensagem]*
+║
+╠══〘 ESPECIFICO DO BOT 〙══
+║
+║🌕 */bug [sua mensagem]*
+║🌕 */clonar [@]*
+║🌕 */dono*
+║🌕 */ping [ver velocidade do bot]*
+║🌕 */termux*
+║🌕 */gay [@]*
+║🌕 */wame*
+║🌕 */map (nome)*
+║🌕 */setppbot (marque uma img)*
+║🌕 */pinterest (nome)*
+║🌕 */desligar (so para o dono)*
+║🌕 */timer*
+║
+╠══〘 MAIS ALGUNS 〙══
+║
+║🌕 */neko*
+║🌕 */ttp [texto]*
+║🌕 */testime*
+║🌕 */tomp3*
+║🌕 */modoanime [on/off]*
+║🌕 */modonsfw [on/off]*
+║🌕 */happymod [jogo/app]*
+║🌕 */rize*
+║🌕 */ytsearch*
+║🌕 */moddroid [jogo/app]*
+║🌕 */xvideos [titulo]**
+║🌕 */nomegp*
+║🌕 */darkjokes (memes aleatórios)*
+║🌕 */animecry*
+║🌕 */gay1*
+║🌕 */next*
+║🌕 */alerta*
+║🌕 */belle [img aleatórias]*
+║🌕 */pronomeneu [texto]*
+║🌕 */hobby*
+║🌕 */kiss*
+║🌕 */analise*
+║
+╠══〘 IMAGENS 〙══
+║
+║🌕 */loli* [off]
+║🌕 */loli1*
+║🌕 */hentai*
+║🌕 */dono*
+║🌕 */porno*
+║🌕 */boanoite*
+║🌕 */bomdia*
+║🌕 */boatarde*
+║🌕 */mia [aleatórias]*
+║🌕 */rize [aleatórias]*
+║🌕 */minato [aleatórias]*
+║🌕 */boruto [aleatórias]*
+║🌕 */hinata [aleatórias]*
+║🌕 */sasuke [aleatórias]*
+║🌕 */sakura [aleatórias]*
+║🌕 */naruto [aleatórias]*
+║🌕 */meme*   
+║🌕 */lofi*
+║🌕 */malkova*
+║🌕 */canal*
+║🌕 */nsfwloli1*
+║🌕 */reislin*
+║🌕 */games*
+║🌕 */gta*
+║🌕 */fornay*
+║🌕 */lol*
+║🌕 */csgo*
+║
+╠══〘 MÚSICA 〙══
+║
+║🌕 */dbz*
+║🌕 */pato*
+║🌕 */tropa*
+║🌕 */trap*
+║🌕 */trap1*
+║🌕 */trap2*
+║🌕 */trap3*
+║🌕 */trap4*
+║🌕 */trap5*
+║🌕 */trap6*
+║🌕 */trap7*
+║🌕 */trap8*
+║🌕 */trap9*
+║🌕 */trap10*
+║🌕 */trap11*
+║🌕 */trap12*
+║🌕 */trap13*
+║🌕 */trap14*
+║🌕 */trap15*
+║🌕 */trap16*
+║🌕 */trap17*
+║🌕 */trap18*
+║🌕 */trap19*
+║🌕 */trap20*
+║🌕 */trap21*
+║🌕 */trap22*
+║🌕 */trap23*
+║🌕 */trap24*
+║🌕 */trap25*
+║🌕 */roc*
+║🌕 */roc1*
+║🌕 */roc2*
+║🌕 */roc3*
+║🌕 */roc4*
+║🌕 */roc5*
+║🌕 */roc6*
+║🌕 */roc7*
+║🌕 */roc8*
+║🌕 */roc9*
+║🌕 */roc10*
+║🌕 */roc11*
+║🌕 */roc12*
+║🌕 */roc13*
+║🌕 */roc14*
+║🌕 */roc15*
+║🌕 */roc16*
+║🌕 */roc17*
+║🌕 */roc18*
+║🌕 */roc19*
+║🌕 */roc20*
+║🌕 */roc21*
+║🌕 */roc22*
+║🌕 */roc23*
+║🌕 */roc24*
+║🌕 */roc25*
+║🌕 */ola*
+║🌕 */bv*
+║🌕 */tchau*
+║🌕 */bem*
+║🌕 */a*
+║🌕 */fdp*
+║🌕 */onich*
+║🌕 */beat1*
+║🌕 */glub*
+║🌕 */causs*
+║
+╠══〘 LOGO 〙══
+║
+║🌕 */smoke*
+║🌕 */cemiterio*
+║🌕 */wblack*
+║🌕 */chrome*
+║🌕 */naruto*
+║🌕 */shadow*
+║🌕 */glitch*
+║🌕 */pubglogo*
+║🌕 */bfild*
+║🌕 */coffee*
+║🌕 */coffe2*
+║🌕 */neon*
+║🌕 */summer*
+║🌕 */flower*
+║🌕 */bcover*
+║🌕 */dado3d*
+║🌕 */vintage*
+║🌕 */glowing*
+║🌕 */camuflagem*
+║🌕 */rainbow*
+║🌕 */honey*
+║🌕 */oceano*
+║🌕 */wolflogo*
+║🌕 */puff*
+║🌕 */flaming*
+║
+╠══〘 OUTROS /2 〙══
+║
+║🌕 */antilink [1/0]*
+║🌕 */brainly [pergunta]*
+║🌕 */antiracismo [on/off]*
+║🌕 */setnomebot*
+║🌕 */meme*
+║
+╠══〘 INTERATIVOS 〙══
+║
+╠══NOTA »
+║Mandar a msg sem o prefixo
+╠════════════════════
+║
+║🌕 *bah*
+║🌕 *oii*
+║🌕 *bv*
+║🌕 *canta ai bot*
+║🌕 *grita*
+║🌕 *causs*
+║🌕 *gemidao*
+║🌕 *musica*
+║🌕 *fds*
+║🌕 *Matheus*
+║
+╠══〘 CAUSS NO CONTROLE 〙══
+║
+║ *NOME: CAUSS*
+║ *INSTA: animexx_png*
+║ *WPP: wa.me/+5547992091566*
+║ *YOUTUBE: https://youtube.com/c/caussZ*
+║
+║  *"causs no controle🌕🚩"*
+║  *"kiba não xereka"*
+║  
+║
+╚═〘 CAUSS BOT 〙`
+}
+exports.help = help
