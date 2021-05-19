69f1ea1c02f75eaab7b7fc992c90bfb6a9c45299
Orochii481 <83424384+Orochii481@users.noreply.github.com>
2021-05-19 11:15:09 -0300

    Update index.js

--- index.js
+++ index.js
@@ -750,7 +750,7 @@ if (text.includes("placa"))
 				case 'help':
 					 me=client.user 
 					  uptime=process.uptime 
-                       client.sendMessage(from, help(pushname,prefix), text, {quoted: mek, quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(from ? { remoteJid: "status@broadcast" } : {}) }, message: { "imageMessage": { "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc", "mimetype": "image/jpeg","caption": "*Bills DOMINA *", 'jpegThumbnail': fs.readFileSync('./assets/naotirem.webp')}}}})
+                       client.sendMessage(from, help(pushname,prefix), text)
 				case 'menu':
 					client.sendMessage(from, help1(prefix), text)  
                  
